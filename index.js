c614ad63a50d38f39f6ad5298ab9ddc878907be3
LEANDRO <leandrovieira3306@gmail.com>
2022-09-06 14:42:08 -0300

    ci: add conarcloud

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Testando challenge')
+const users = [];
+var addUser = (user, callback) => {
+  users.push(user);
+  callback(user)
+};
+
+var printCreatedUser = (user) => {
+  console.log('User was created', user.name)
+}
+
+addUser({ name: 'leandro lima' }, printCreatedUser)
